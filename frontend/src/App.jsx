ce7e13454861b92d19c5373f220d97966bd9ad22
Misbah Juwayriyyah <misbahjaveria32@gmail.com>
2024-08-23 00:25:13 +0500

    frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,21 @@
+// React project using vite package. Vite JS is majorly used to set up a development environment for various frameworks like React and Vue, and even for a Vanilla JavaScript app with a dev server and hot reloading by following just three commands. Vite is a bridge between Twig/Craft CMS and the next-generation frontend build tool Vite. js.
+// --npm create vite@latest
+// Project Name : frontend
+// Framework : react
+// variant : Js
+// -> frontend -> --npm install (to go front vite to react)
+// Dependencies:
+//     React Router Dom
+//     React Toastify (toast notifications)
+//Install ES& React etc. extension
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      hi
+    </div>
+  )
+}
+
+export default App
